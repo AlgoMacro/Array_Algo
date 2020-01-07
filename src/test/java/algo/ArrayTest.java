f5e6ac76ca076a34a417c33d265268f7ca6c13d4
macro <18835118106@163.com>
2020-01-07 19:47:31 +0800

    数组遍历

--- src/test/java/algo/ArrayTest.java
+++ src/test/java/algo/ArrayTest.java
@@ -37,5 +37,28 @@ public class ArrayTest {
         System.out.println("int1="+Arrays.toString(int1));
     }
 
+    @Test
+    public void testBianli(){
+        Integer[] arr = {1,2,3,4,5};
+        for (int index=0;index<arr.length;index++){
+            //System.out.println("下标"+index+"值："+arr[index]);
+            System.out.println(String.format("下标：%d,存放的值是：%d",index,arr[index]));
+        }
+        int count=0;
+        for (int val:arr) {
+            System.out.println("下标"+count+"值："+arr[count]);
+            count++;
+        }
+        Arrays.asList(arr).forEach(val -> {
+            System.out.println("值是："+val);
+            if(val%2==0){
+                System.out.println("它是一个偶数");
+            }
+            else {
+                System.out.println("它是一个奇数");
+            }
+        });
+
+    }
 
 }
